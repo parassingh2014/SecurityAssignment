7a5d1cf471719e3bcba6d391dae86fcc582a1139
Ramandeep Marwaha <43007109+parassingh2014@users.noreply.github.com>
2022-10-14 11:27:12 -0600

    Security

---
+++ BAIS3110_ASPLab/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BAIS3110_ASPLab.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public DateTime Message { get; set; }
+        public string Name { get; set; }
+
+        [BindProperty]
+        public string InputTextBox { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+
+
+            for (var i = 0; i<=5; i++)
+            {
+                Name = "Raman";
+            }
+        }
+
+        public void OnPost()
+        {
+            Message = DateTime.Now;
+        }
+    }
+}
